a26c65766743ac87824f24af9f1471142422e257
SLEAZOIDS <vegetarian304@gmail.com>
2019-04-27 13:07:29 +0900

    chapter ten complete

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,14 +1,23 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user       = users(:michael)
+  end
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", users_path, count: 0
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     get contact_path
     assert_select "title", full_title("Contact")
+    log_in_as(@user)
+    follow_redirect!
+    assert_select "a[href=?]", users_path, count: 1
   end
 end
